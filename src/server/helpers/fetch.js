2f32bde0d51df8393b01f8f652d7a67080a0a94e
TheJang <33803975+jangwonyoon@gmail.com>
2020-04-29 18:29:34 +0900

    helpers fetch clear

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -2,6 +2,23 @@ const https = require("https");
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+
+  return new Promise((resolve, reject) => {
+    try {
+      let body = "";
+      https.get(url, (res) => {
+        res
+          .on("data", (chunk) => {
+            body = body + chunk.toString();
+          })
+          .on("end", () => {
+            resolve(body);
+          });
+      });
+    } catch (error) {
+      reject("this is error", error);
+    }
+  });
 }
 
 module.exports = {
