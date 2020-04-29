b3a1ea5bb2bd8dfd9c6ea7e94199221790ad5ccd
TheJang <33803975+jangwonyoon@gmail.com>
2020-04-29 18:49:13 +0900

    source.js completed

--- src/server/routes/source.js
+++ src/server/routes/source.js
@@ -6,11 +6,15 @@ const router = express.Router();
 // GET /source
 router.get("/", async (req, res) => {
   // TODO: Help function을 이용하여, source.txt의 내용을 반환 수 있도록 구현하세요.
+  const html = await fileHelper.readFile("./data/source.txt");
+  res.status(200).send(html);
 });
 
 // POST /source
 router.post("/", async (req, res) => {
   // TODO: Help function을 이용하여, source.txt의 내용으로 저장할 수 있도록 구현하세요.
+  const body = await fileHelper.writeFile("./data/source.txt", req.body);
+  res.status(200).send("ok");
 });
 
 module.exports = router;
