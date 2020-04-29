ad10e59f736a53665bb10535a5f1e50ea9a07aa0
TheJang <33803975+jangwonyoon@gmail.com>
2020-04-29 18:41:15 +0900

    file.js completed

--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -1,14 +1,22 @@
 const fs = require("fs");
 
 async function writeFile(filename, body) {
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
+    fs.writeFile(filename, body, (err) => {
+      if (err) return reject(err);
+      return resolve(body);
+    });
   });
 }
 
 async function readFile(filename) {
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) return reject(err);
+      return resolve(data);
+    });
   });
 }
 
@@ -21,8 +29,12 @@ async function writeSourceListFile(body) {
 }
 
 async function readLineFromSourceList(nthline) {
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     // TODO : ./data/source.txt에 저장되어 있는 텍스트에서 특정 줄에 해당하는 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile("./data/source.txt", "utf8", (err, data) => {
+      if (err) return reject(err);
+      return resolve(data.split("\n")[nthline]);
+    });
   });
 }
 
